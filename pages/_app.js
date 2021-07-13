4013cac848653b03afc8f244e91c0a71ec945e51
Bruna Nunes Pereira <brunacris.2404@gmail.com>
2021-07-13 04:09:31 -0300

    Projeto Alurakut

---
+++ pages/_app.js
@@ -0,0 +1,51 @@
+import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import {AlurakutStyles} from './src/lib/AlurakutCommons'
+
+const GlobalStyle = createGlobalStyle`
+  /*Reset CSS (Necolas Reset CSS <3)*/
+  *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+    background: #D9E6F6;
+  }
+
+  #__next{
+    display: flex;
+    min-height: 100vh;
+    flex-direction: column;
+  }
+
+  img {
+    max-width: 100%;
+    height: auto;
+    display: block;
+    border-radius: 4px;
+  }
+
+  ${AlurakutStyles}
+`
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  )
+}
